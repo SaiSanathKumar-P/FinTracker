0bd30bb48757eca84c7a4e147bec97697b35e09f
SaiSanathKumar-P <saisanathkumar140@gmail.com>
2026-02-14 20:17:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
-const authRoutes = require("./routes/auth");
-const expenseRoutes = require("./routes/expenses");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-app.use(express.urlencoded({ extended:true }));
+app.use(express.static("public"));
 
-app.use("/api/auth", authRoutes);
-app.use("/api/expenses", expenseRoutes);
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/expenses", require("./routes/expenses"));
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
-
-app.get("/", (req,res)=>{
-  res.send("FinTrack Backend Running");
-});
+.then(()=> console.log("MongoDB Connected Successfully"))
+.catch(err=> console.log(err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server started"));
 
+app.listen(PORT, ()=>{
+  console.log(`Server running on https://fintrackerr.onrender.com:${PORT}`);
+});

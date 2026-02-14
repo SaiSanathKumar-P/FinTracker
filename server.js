22542d7d42e336ddf7cfa23f1c3277990f26d3db
SaiSanathKumar-P <saisanathkumar140@gmail.com>
2026-02-14 21:11:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use("/api/auth", require("./routes/auth"));
@@ -21,3 +22,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
   console.log(`Server running on https://fintrackerr.onrender.com:${PORT}`);
 });
+

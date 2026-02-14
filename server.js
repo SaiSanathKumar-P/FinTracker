1598a9b4a091d7b70e73e13bf55b4a97ee229085
SaiSanathKumar-P <saisanathkumar140@gmail.com>
2026-02-14 19:53:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
+
+const authRoutes = require("./routes/auth");
+const expenseRoutes = require("./routes/expenses");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
 
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/expenses", require("./routes/expenses"));
+app.use("/api/auth", authRoutes);
+app.use("/api/expenses", expenseRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
-.then(()=> console.log("MongoDB Connected Successfully"))
-.catch(err=> console.log(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/", (req,res)=>{
+  res.send("FinTrack Backend Running");
 });
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server started"));

9b5dcd89036b0067ad2a3323df8be99beffd3c5c
SaiSanathKumar-P <saisanathkumar140@gmail.com>
2026-02-14 21:53:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,16 +18,10 @@ app.use("/api/expenses", expenseRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected Successfully"))
-.catch(err => console.log("MongoDB Error:", err));
+.catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
-
-
-
-

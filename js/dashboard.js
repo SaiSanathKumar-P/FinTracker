90d2a6d8a54b007f88ae4bb981a5057d94f2a7f3
SaiSanathKumar-P <saisanathkumar140@gmail.com>
2026-02-14 22:31:25 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -72,7 +72,7 @@ async function loadExpenses() {
   try {
 
     const res = await fetch(EXPENSE_API, {
-      headers: { "Authorization": token }
+      headers: { "Authorization": `Bearer ${token}` }
     });
 
     if (!res.ok) {
@@ -122,7 +122,8 @@ async function deleteExpense(id) {
 
   await fetch(EXPENSE_API + "/" + id, {
     method: "DELETE",
-    headers: { "Authorization": token }
+    headers: { "Authorization": `Bearer ${token}` }
+
   });
 
   loadExpenses();
@@ -165,3 +166,4 @@ function logoutUser() {
 // INITIAL LOAD
 loadExpenses();
 loadSmartAnalysis();
+

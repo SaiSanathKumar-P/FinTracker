7bcfd3e67424595dc19b2a7e15919a464a29737d
SaiSanathKumar-P <saisanathkumar140@gmail.com>
2026-02-14 14:14:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/expenses", require("./routes/expenses"));
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
 .then(()=> console.log("MongoDB Connected Successfully"))
 .catch(err=> console.log(err));
 
@@ -21,3 +21,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
   console.log(`Server running on http://localhost:${PORT}`);
 });
+

2bd924c7955cb926b455b3efdac67c5c6b734edc
SaiSanathKumar-P <saisanathkumar140@gmail.com>
2026-02-14 20:02:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const expenseRoutes = require("./routes/expenses");
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/expenses", expenseRoutes);
@@ -23,3 +25,4 @@ app.get("/", (req,res)=>{
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server started"));
+

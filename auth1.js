0edf911b6799b477f7ddb1bdcce49469ac68cdb1
SaiSanathKumar-P <saisanathkumar140@gmail.com>
2026-02-14 16:10:19 +0530

    Update auth1.js

--- auth1.js
+++ auth1.js
@@ -9,7 +9,7 @@ if (loginForm) {
     const password = document.getElementById("password").value;
 
     try {
-      const response = await fetch("https://fintrackerr.onrender.com/api/auth1/login", {
+      const response = await fetch("https://fintrackerr.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
@@ -48,7 +48,7 @@ if (registerForm) {
     const password = document.getElementById("password").value;
 
     try {
-      const res = await fetch("https://fintrackerr.onrender.com/api/auth1/register", {
+      const res = await fetch("https://fintrackerr.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"

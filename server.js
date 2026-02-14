224350a205677c7bd39203d60c1ab5af84487b73
SaiSanathKumar-P <saisanathkumar140@gmail.com>
2026-02-14 21:29:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,14 +14,15 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/expenses", require("./routes/expenses"));
 
 mongoose.connect(process.env.MONGO_URI)
-.then(()=> console.log("MongoDB Connected Successfully"))
-.catch(err=> console.log(err));
+.then(() => console.log("MongoDB Connected Successfully"))
+.catch(err => console.log("MongoDB Error:", err));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
-  console.log(`Server running on https://fintrackerr.onrender.com:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
 
 
 

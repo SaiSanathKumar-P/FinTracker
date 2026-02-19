3e437e5b588f82df110d62e641ae18a179e1828d
SaiSanathKumar-P <sytchannel11@gmail.com>
2026-02-19 22:00:13 +0530

    Update dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -734,4 +734,7 @@ document.addEventListener("DOMContentLoaded", () => {
   requireAuth();
   initDashboard();
 });
-
+window.history.pushState(null, null, window.location.href);
+window.onpopstate = function () {
+  window.history.go(1);
+};

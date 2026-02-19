6082218692c8b61ea3c2827bb7312ff1a8f1bb90
SaiSanathKumar-P <sytchannel11@gmail.com>
2026-02-19 15:48:22 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function(req, res, next) {
-  const token = req.header("Authorization")?.split(" ")[1];
+const authHeader = req.headers.authorization || req.headers.Authorization;
+const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ message: "No token" });

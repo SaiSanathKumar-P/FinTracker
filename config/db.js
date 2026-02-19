a46df1fbfd4cc93871f654d5b8f7da42fe42bb12
SaiSanathKumar-P <sytchannel11@gmail.com>
2026-02-19 21:51:47 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,9 +5,7 @@ let isConnected = false;
 async function connectDB() {
   if (isConnected) return;
 
-  await mongoose.connect(process.env.MONGO_URI, {
-    bufferCommands: false,
-  });
+  await mongoose.connect(process.env.MONGO_URI);
 
   isConnected = true;
   console.log("MongoDB Connected");
